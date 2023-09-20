9d4fe8890a101ee242f562d045e4ad486616a0fe
Emanuel <v2.lorenc@gmail.com>
2023-09-20 18:52:32 +0200

    fixing sidebar menu

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,10 @@
 # Your existing imports
 import streamlit as st
-from pages import home_page, data_catalogue_page, data_processing_page, data_analytics_page, data_visualization_page
+import home_page 
+import data_catalogue_page 
+import data_processing_page 
+import data_analytics_page
+import data_visualization_page
 
 # import machine_learning_page
 # import llm_page  # Import the new LLM page
@@ -8,6 +12,10 @@ from utils import custom_css
 
 # Add custom CSS for centered title
 custom_css.add_css()
+
+# Path to the local image
+image_path = './utils/logo.jpg'  # Replace 'your_logo.jpg' with the actual file name
+
 
 # Reordered the pages for logical flow and added LLM page
 PAGES = {
@@ -25,6 +33,7 @@ if 'page' not in st.session_state:
     st.session_state.page = "🏠 Home"
 
 with st.sidebar:
+    st.image(image_path, caption='', use_column_width=True)  # Display the image
     st.markdown("<h1 style='text-align: left;'>👋 Welcome</h1>", unsafe_allow_html=True)
 
     # Create buttons for navigation, reordered for logical flow
@@ -38,10 +47,10 @@ with st.sidebar:
         st.session_state.page = "📊 Data Analytics"
     if st.sidebar.button("📈 Data Visualization"):
         st.session_state.page = "📈 Data Visualization"
-    # if st.sidebar.button("🤖 Machine Learning"):
-    #     st.session_state.page = "🤖 Machine Learning"
-    # if st.sidebar.button("🦙 LLM"):  # New LLM button with llama emoji
-    #     st.session_state.page = "🦙 LLM"
+    if st.sidebar.button("🤖 Machine Learning"):
+        st.session_state.page = "🤖 Machine Learning"
+    if st.sidebar.button("🦙 LLM"):  # New LLM button with llama emoji
+        st.session_state.page = "🦙 LLM"
 
 # Load the selected page
 PAGES[st.session_state.page].app()
