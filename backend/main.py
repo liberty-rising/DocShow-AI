3ae00dc6768d71a155cc05e90153176cfc9d5ac0
liberty-rising <rwyszomi@gmail.com>
2023-10-04 23:55:18 +0200

    work on upload workflow

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
-from utils import call_llm_api, process_file
+from utils import create_or_append_llm_table, process_file, save_to_data_lake
 
 app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World"}
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...), msg: str = "", is_new_table: bool = False):
@@ -21,16 +18,21 @@ async def upload_file(file: UploadFile = File(...), msg: str = "", is_new_table:
     Returns:
         JSONResponse: A JSON response containing either a success message and result or an error message.
     """
-    # Process file to string
-    sample_content = process_file(file)
-    
-    result = call_llm_api(sample_content,msg,is_new_table)
-    
-    if result:
-        return JSONResponse(content={"message": "File processed successfully", "result": result})
-    else:
-        return JSONResponse(content={"message": "Error processing file"}, status_code=400)
+    try:
+        # Process the file to get the sample content
+        sample_content = process_file(file)
+        
+        # Call the LLM API
+        llm_result = create_or_append_llm_table(sample_content, msg, is_new_table)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+        # Optionally, save the file to a data lake
+        save_to_data_lake(file)
+    
+        if llm_result:
+            return JSONResponse(content={"message": "File processed successfully", "result": llm_result})
+        else:
+            raise HTTPException(status_code=400, detail="Error processing file with LLM API")
+    
+    except Exception as e:
+        # General error handling
+        return JSONResponse(content={"message": f"An error occured: {str(e)}"}, status_code=500)
--- backend/utils.py
+++ backend/utils.py
@@ -4,6 +4,7 @@ from typing import Any
 
 import json
 import pandas as pd
+import re
 import requests
 
 def api_request(prompt, url, headers):
@@ -22,21 +23,39 @@ def api_request(prompt, url, headers):
         })
     return requests.post(url, headers=headers, data=payload)
 
-def call_llm_api(file_content, msg, is_new_table):
+def create_or_append_llm_table(file_content: str, msg: str, is_new_table: bool):
+    try:
+        llm_url,headers = get_api_credentials()
+
+        if is_new_table:
+            sql_response = generate_create_table_statement(file_content,msg,llm_url,headers)
+            
+            if sql_response.status_code != 200:
+                raise Exception("Failed to generate SQL table statement. LLM API returned non-200 status code.")
+            
+            sql_query = extract_sql_query(sql_response.json()["output"])
+
+            # Execute query
+
+            # Append original data to table
+            
+            return sql_response.json()
+        
+        else:
+            # Code for appending data to existing tables
+            pass
+    
+    except Exception as e:
+        # Log the error message here
+        print(f"An error occurred: {str(e)}")
+        return None
+
+def get_api_credentials():
     llm_url = "https://api.runpod.ai/v2/2btspg14jnwza1/runsync"
     headers = {
         "Authorization": "Bearer 05IB5U9J6DD7UG8GXO0X1DU2M68JF5AZODD5JC1J",
         "Content-Type": "application/json"}
-
-    if is_new_table:
-        sql_response = generate_create_table_statement(file_content,msg,llm_url,headers)
-    
-    print("SQL: ",sql_response.json())
-    return None
-    # if response.status_code == 200:
-    #     return response.json()
-    # else:
-    #     return None
+    return llm_url, headers
     
 def generate_create_table_statement(file_content, msg, llm_url, headers):
     """
@@ -51,10 +70,10 @@ def generate_create_table_statement(file_content, msg, llm_url, headers):
     Returns:
         tuple: API responses for SQL statement and description.
     """
-    sql_context = f"You are specialized in generating SQL table creation statements based on\
-          provided sample data. Ensure the statements are correctly formatted and adhere to SQL syntax standards.\
-          Only provide the SQL response, nothing else. Do not acknowledge the questions or tasks in your answer."
-    sql_prompt = f"Create a SQL table statement for this sample data: {file_content}"
+    sql_context = "Generate only a SQL CREATE TABLE statement based on the provided sample data. Do not include any additional text, explanations, or filler words."
+    sql_prompt = f"Generate SQL CREATE TABLE statement for the following sample data: {file_content}.\
+        Do not include any additional text, explanations, or filler words."
+
     if msg:
         sql_prompt += f" Additional information about data: {msg}"
     full_prompt = prompt_format(sql_context,sql_prompt)
@@ -95,3 +114,16 @@ def process_file(file: UploadFile) -> Any:
 def prompt_format(context: str, prompt: str):
     full_prompt = f"""[INST] <<SYS>>{context}<</SYS>>{prompt}[/INST]"""
     return full_prompt
+
+def extract_sql_query(text):
+    """
+    Created due to limited capability of Llama-2-13B-chat-GPTQ being able to return pure sql without extra words.
+    """
+    match = re.findall(r'CREATE TABLE [^;]+;', text)
+    if match:
+        # Extract only the last "CREATE TABLE" statement and add a space after "CREATE TABLE"
+        last_statement = match[-1]
+        return "CREATE TABLE " + last_statement.split("CREATE TABLE")[-1].strip()
+    
+def save_to_data_lake(file: UploadFile = File(...)):
+    pass
