4ded34e73659cbe7ee7422f620b4fd6d689308ac
Emanuel <v2.lorenc@gmail.com>
2023-12-02 14:08:10 +0100

    adding Snackbar message after new organization creation

--- frontend/src/pages/Admin/OrganizationManagement.jsx
+++ frontend/src/pages/Admin/OrganizationManagement.jsx
@@ -2,21 +2,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../utils/constants.jsx';
-import { Button, Card, CardContent, TextField, Typography } from '@mui/material';
+import { Button, Card, CardContent, TextField, Typography, Snackbar, Alert } from '@mui/material';
 
 function OrganizationManagement() {
   const [organizationName, setOrganizationName] = useState('');
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState('success'); // 'error' or 'success'
 
   const handleAddOrganization = () => {
     axios.post(`${API_URL}organizations/add`, { name: organizationName })
       .then(response => {
         // Handle successful addition
         console.log('Organization added:', response.data);
+        setSnackbarMessage('Organization added successfully.');
+        setSnackbarSeverity('success');
+        setOpenSnackbar(true);
         setOrganizationName(''); // Reset the input field
       })
       .catch(error => {
         // Handle error
         console.error('Error adding organization', error);
+        setSnackbarMessage('Error adding organization.');
+        setSnackbarSeverity('error');
+        setOpenSnackbar(true);
       });
   };
 
@@ -39,6 +48,13 @@ function OrganizationManagement() {
           Add Organization
         </Button>
       </CardContent>
+
+      {/* Snackbar for showing notifications */}
+      <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={() => setOpenSnackbar(false)}>
+        <Alert onClose={() => setOpenSnackbar(false)} severity={snackbarSeverity} sx={{ width: '100%' }}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </Card>
   );
 }
