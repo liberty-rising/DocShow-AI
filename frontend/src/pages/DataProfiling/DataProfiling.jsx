110e0d9ca7a43baab032325684beccd434b91d99
Rezart Abazi <rezart392@gmail.com>
2023-12-04 19:13:58 +0100

    add extract data button

--- frontend/src/pages/DataProfiling/DataProfiling.jsx
+++ frontend/src/pages/DataProfiling/DataProfiling.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, Typography, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import { Box, Typography, Select, MenuItem, FormControl, InputLabel, Button } from '@mui/material';
 
 function DataProfilingPage() {
   const [dataProfiles, setDataProfiles] = useState([]);
   const [selectedProfile, setSelectedProfile] = useState('');
 
   useEffect(() => {
-    axios.get('https://127.0.0.1/data-profiling')
+    axios.get('https://127.0.0.1/data-profiling') // Replace with your actual backend URL
       .then(response => {
         setDataProfiles(response.data);
       })
@@ -18,11 +18,22 @@ function DataProfilingPage() {
     setSelectedProfile(event.target.value);
   };
 
+  const handleExtractData = () => {
+    axios.post('https://127.0.0.1/data-profiling', { profileId: selectedProfile }) // Replace with your actual backend URL
+      .then(response => {
+        // Handle the response
+        console.log('Data extracted:', response.data);
+      })
+      .catch(error => {
+        console.error('Error extracting data:', error);
+      });
+  };
+
   return (
     <Box>
       <Typography variant="h4" gutterBottom>🔍 Data Profiling</Typography>
       <FormControl fullWidth>
-        <InputLabel id="data-profile-select-label">Data Profile</InputLabel>
+        <InputLabel id="data-profile-select-label">Select all the elements to extract</InputLabel>
         <Select
           labelId="data-profile-select-label"
           id="data-profile-select"
@@ -37,6 +48,14 @@ function DataProfilingPage() {
           ))}
         </Select>
       </FormControl>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleExtractData}
+        sx={{ mt: 2 }}
+      >
+        Extract Data
+      </Button>
       {/* Other content */}
     </Box>
   );
