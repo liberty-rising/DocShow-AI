a6af1739db660a70418edf45e1cbcf02ca681c98
liberty-rising <rwyszomi@gmail.com>
2023-12-05 17:33:00 +0100

    remove unnecessary logging

--- frontend/src/components/Auth/RequireAuth.jsx
+++ frontend/src/components/Auth/RequireAuth.jsx
@@ -4,13 +4,9 @@ import { useAuth } from '../../contexts/AuthContext';
 function RequireAuth({ children }) {
   const { isAuthenticated, isLoading } = useAuth();
 
-  console.log(`loading ${isLoading}`)
-
   if (isLoading) {
     return <div>Loading...</div>; // Or your preferred loading indicator/component
   }
-
-  console.log(`isAuthenticated ${isAuthenticated}`)
 
   if (!isAuthenticated) {
     // Redirect to the login page if not authenticated
