e11b65ffe80543cc0ee1cfc66443115f709eb791
liberty-rising <rwyszomi@gmail.com>
2023-12-10 21:32:31 +0100

    create table model for mapping tables to organizations

---
+++ backend/models/organization_table_map.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String
+
+from .base import Base
+
+
+class OrganizationTableMap(Base):
+    """
+    Represents a mapping between an organization and a table.
+
+    Attributes:
+        id (int): The unique identifier of the mapping.
+        organization_id (int): The ID of the organization.
+        table_name (str): The name of the table.
+        table_alias (str): The alias for the table.
+    """
+
+    __tablename__ = "organization_table_map"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    organization_id = Column(Integer)
+    table_name = Column(String)
+    table_alias = Column(String)
