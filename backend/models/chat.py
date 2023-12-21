4511a9594cf50803c4279f42a20eec49e11edd5a
liberty-rising <rwyszomi@gmail.com>
2023-12-21 19:54:25 +0100

    integrate langchain into analytics

--- backend/models/chat.py
+++ backend/models/chat.py
@@ -1,10 +1,9 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import Optional
 
 
 class AnalyticsRequest(BaseModel):
     chat_id: Optional[int]
-    table_names: List[str]
     prompt: str
 
 
