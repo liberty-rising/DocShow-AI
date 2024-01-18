7ac7b8282fe317dc250ba3c7b6702ea547e65e20
liberty-rising <rwyszomi@gmail.com>
2024-01-18 21:16:19 +0100

    integrate connection pooling

--- backend/llms/gpt_lang.py
+++ backend/llms/gpt_lang.py
@@ -5,14 +5,28 @@ from langchain.agents.agent_toolkits import SQLDatabaseToolkit
 from langchain.agents.agent_types import AgentType
 from langchain.llms.openai import OpenAI
 from langchain.sql_database import SQLDatabase
-from settings import DATABASE_URL
+from settings import (
+    APP_ENV,
+    DATABASE_LANGCHAIN_POOL_MAX_OVERFLOW,
+    DATABASE_LANGCHAIN_POOL_SIZE,
+    DATABASE_POOL_URL,
+    DATABASE_URL,
+)
 
 
 class GPTLangSQL:
     def __init__(self, tables: List[str]):
         if not tables:
             raise ValueError("No tables provided")
-        self.db = SQLDatabase.from_uri(DATABASE_URL, include_tables=tables)
+        if APP_ENV == "prod":
+            self.db = SQLDatabase.from_uri(
+                DATABASE_POOL_URL,
+                include_tables=tables,
+                pool_size=DATABASE_LANGCHAIN_POOL_SIZE,
+                max_overflow=DATABASE_LANGCHAIN_POOL_MAX_OVERFLOW,
+            )
+        else:
+            self.db = SQLDatabase.from_uri(DATABASE_URL, include_tables=tables)
         self.toolkit = SQLDatabaseToolkit(db=self.db, llm=OpenAI(temperature=0))
         self.agent_executor = create_sql_agent(
             llm=OpenAI(temperature=0),
